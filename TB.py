335c3801ff02c01fc04c99ac1f1b6c10d8cc0a07
HanphoneJan <1195560097@qq.com>
2024-10-31 11:41:50 +0800

    Changes

--- TB.py
+++ TB.py
@@ -1,13 +1,18 @@
-import datetime
 import time
+import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By  # 加载所需的库
 
 # 首先我们需要设置抢购的时间，格式要按照预设的格式改就可以，个月数的一定在前面加上0，例如 “01”
 now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-mstime = "2024-10-31 15:36:00.000000"
+mstime = "2024-10-31 11:34:00.000000"
 #print(mstime)
-mstime = input("请输入时间: ")
+#mstime = input("请输入时间: ")
+
+#预留刷新页面的时间
+mstime_datetime = datetime.datetime.strptime(mstime, "%Y-%m-%d %H:%M:%S.%f")
+mstime_datetime = mstime_datetime - datetime.timedelta(seconds=0.5)
+mstime = mstime_datetime.strftime('%Y-%m-%d %H:%M:%S.%f')
 
 # 选择使用的浏览器，如果没有Chrome浏览器可以更改其他浏览器，需要driver
 WebBrowser = webdriver.Chrome()
@@ -47,17 +52,22 @@ print(f"到达预定时间将自动开始抢购")
 
 while True:
     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-    #print(now)
+    print(now)
     if now >= mstime:
         # 当当前时间超过了抢购时间就立刻执行下面代码
         while True:
             try:
-                if WebBrowser.find_element(By.CLASS_NAME, "btn--QDjHtErD"):
-                    WebBrowser.find_element(By.CLASS_NAME,"btn--QDjHtErD").click()
-                    print(f"抢购成功，请尽快付款")
-                    print(now)
-                    break
+                WebBrowser.refresh()
+                WebBrowser.find_element(By.CLASS_NAME,"btn--QDjHtErD").click()
+                print(f"抢购成功，请尽快付款")
+                now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+                print(now)
+                print(f"5秒后自动关闭程序")
+                time.sleep(5)
+                break
             except:
                 print(f"抢购失败")
                 break
-        time.sleep(0.01)
+        time.sleep(0.001)
+        break
+exit()
